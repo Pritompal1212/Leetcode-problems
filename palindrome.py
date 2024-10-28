0540b97e4ed642282a6d327eeb91f2df466234ff
Pritompal1212 <mspritom111@gmail.com>
2024-10-28 16:26:26 +0530

    added

--- palindrome.py
+++ palindrome.py
@@ -1,8 +1,9 @@
-class Solution(object):
-    def isPalindrome(x):
-        lst=[x]
-        a=lst.reverse()
-        if (a==lst):
-            return print("it is Palindrome")
-            
-    print(isPalindrome(121))
+def ispalindrome(x):
+    convert_string=str(x)
+    if convert_string == convert_string[::-1]:      #[::-1] = [start:stop:step]  basically for -1 it reversed a string
+        print("its a palindrome")
+    else:
+        print("not a palindrome")
+
+ispalindrome(10)
+        
