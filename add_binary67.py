6f13ac774b22bcc63d23fa2dddbc7a507a78db3f
Pritompal1212 <mspritom111@gmail.com>
2024-10-29 13:28:03 +0530

    done

---
+++ add_binary67.py
@@ -0,0 +1,31 @@
+def add_binary(a,b):
+    r=bin(int(a,2)+int(b,2))[2:]
+    return r
+
+a="1010"
+b="1011"
+r1=add_binary(a,b)
+print(r1)
+
+
+'''  
+class Solution:
+  def addBinary(self, a: str, b: str) -> str:
+    s = []
+    carry = 0
+    i = len(a) - 1
+    j = len(b) - 1
+
+    while i >= 0 or j >= 0 or carry:
+      if i >= 0:
+        carry += int(a[i])
+        i -= 1
+      if j >= 0:
+        carry += int(b[j])
+        j -= 1
+      s.append(str(carry % 2))
+      carry //= 2
+
+    return ''.join(reversed(s))
+
+'''
