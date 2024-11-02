6f1e104e317b5ccb613502d541f39f3ffebb7d72
Pritompal1212 <mspritom111@gmail.com>
2024-11-02 22:32:46 +0530

    ok

--- median_sorted_array4.py
+++ median_sorted_array4.py
@@ -1,24 +1,53 @@
 # HARD 
+ 
+#but its not a ligalmethod
 # def merge_sorted_arr(nums1, nums2):
-#     arr = [x for x in nums1 if x != 0] + nums2
-#     arr.sort()
-#     return arr
-# nums1 = [1,3]
-# nums2 = [2]
+#     if len(nums1) < len(nums2):
+#         nums2.extend(nums1)
+#         nums2.sort()
+#         mid1=(nums2[0]+nums2[-1])/2
+#         return mid1
+#     else:
+#         nums1.extend(nums2)
+#         nums1.sort()
+#         mid2=(nums1[0]+nums1[-1])/2
+#         return mid2
+    
+# nums1 = [1,2]
+# nums2 = [3,4]
 # print(merge_sorted_arr(nums1, nums2)) 
 
-def merge_sorted_arr(nums1, nums2):
-    if len(nums1) < len(nums2):
-        nums2.extend(nums1)
-        nums2.sort()
-        mid1=(nums2[0]+nums2[-1])/2
-        return mid2
-    else:
-        nums1.extend(nums2)
-        nums1.sort()
-        mid2=(nums1[0]+nums1[-1])/2
-        return mid2   
-    
+
+#my one
+# def findMedianSortedArrays(nums1, nums2):
+#         nums = nums1 + nums2
+#         nums = sorted(nums)
+#         mid=len(nums)//2
+
+#         if len(nums) %2 != 0:
+#             return nums[mid]
+#         else:
+#             mid=(nums[mid-1] + nums[mid])
+#             return mid / 2.0
+# nums1 = [1,2]
+# nums2 = [3,4]
+# print(findMedianSortedArrays(nums1, nums2)) 
+
+
+#best run time
+
+def findMedianSortedArrays(nums1, nums2):
+        sum = 0
+        merge = nums1 + nums2
+        merge.sort()
+        n = len(merge)
+        if (n % 2 == 0):
+            mid1 = merge[n // 2 - 1]
+            mid2 = merge[n // 2]
+            median = (mid1 + mid2) / 2.0
+        else:
+            median = merge[n // 2]
+        return median
 nums1 = [1,2]
 nums2 = [3,4]
-print(merge_sorted_arr(nums1, nums2)) 
+print(findMedianSortedArrays(nums1, nums2)) 
