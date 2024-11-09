5e43bee5fee0ce11ffbd8b6af151e43ed01bce00
Pritompal1212 <mspritom111@gmail.com>
2024-11-09 21:32:41 +0530

    ok

---
+++ subsets78.py
@@ -0,0 +1,23 @@
+def subsets(nums):
+    result = []
+
+    def backtrack(start, path):
+        # Add the current subset to the result
+        result.append(path[:])
+        
+        # Explore further subsets
+        for i in range(start, len(nums)):
+            # Include the current element
+            path.append(nums[i])
+            # Move to the next element
+            backtrack(i + 1, path)
+            # Backtrack: remove the last element
+            path.pop()
+
+    # Start backtracking from the first index
+    backtrack(0, [])
+    return resultgit ad
+
+# Test the function
+nums = [1, 2, 3]
+print(subsets(nums))
