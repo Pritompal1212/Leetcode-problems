3faec22174830f86378a57b2f76435b1782f9f44
Pritompal1212 <mspritom111@gmail.com>
2024-12-01 10:34:27 +0530

    275th

---
+++ H_indextwo275.py
@@ -0,0 +1,15 @@
+def hIndex(citations):
+    n = len(citations)
+    left, right = 0, n - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if citations[mid] == n - mid:
+            return n - mid
+        elif citations[mid] < n - mid:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return n - left
+        
+citations = [1,2,100]
+print(hIndex(citations))
