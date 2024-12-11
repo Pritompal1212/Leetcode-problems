aa319b078df5dba1877971c1eed55799acdeace5
Pritompal1212 <mspritom111@gmail.com>
2024-12-11 22:38:01 +0530

    819th

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,11 +1,11 @@
-l=0
-    r=len(nums)-1
-    while l<r:
-        mid=(l+r)//2
+def mostCommonWord(paragraph, banned):
+#     paragraph = " ".join(re.findall(r"[a-zA-Z0-9]+", paragraph))
+#     c = Counter(paragraph.lower().split())
+#     c = OrderedDict(c.most_common())
+#     for word in c:
+#         if word not in banned:
+#             return word
         
-        if nums[mid] <= nums[r]:
-            r=mid
-        else:
-            l=mid+1
-        
-    return nums[l]
+# paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
+# banned = ["hit"]
+# print(mostCommonWord(paragraph, banned))
